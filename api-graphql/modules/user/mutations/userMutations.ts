5beb134e48bff3c94b0d8629ae9471abc7c02662
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-04-06 00:21:14 -0300

    feat: add user mutations

---
+++ api-graphql/modules/user/mutations/userMutations.ts
@@ -0,0 +1,65 @@
+// user mutation file
+import { GraphQLObjectType, GraphQLString } from "graphql";
+import UserType from "../userType";
+import UserModel from "../userModel";
+
+const UserMutation = new GraphQLObjectType({
+  name: "UserMutation",
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        name: { type: GraphQLString },
+        birth_date: { type: GraphQLString },
+        mother_name: { type: GraphQLString },
+        phone: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        const user = new UserModel({
+          name: args.name,
+          birth_date: args.birth_date,
+          mother_name: args.mother_name,
+          phone: args.phone,
+        });
+        return user.save();
+      },
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        birth_date: { type: GraphQLString },
+        mother_name: { type: GraphQLString },
+        phone: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return UserModel.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              birth_date: args.birth_date,
+              mother_name: args.mother_name,
+              phone: args.phone,
+            },
+          },
+          { new: true }
+        );
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return UserModel.findByIdAndDelete(args.id)
+        .then(deletedUser => deletedUser)
+        .catch(err => console.log(err));
+      }
+    }
+  },
+});
+
+export default UserMutation;
