92fcbe6d5e55c6c8e91a4971a3ab05e9a9f84377
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-04-14 00:06:10 -0300

    feat: set cors

--- api-graphql/server.ts
+++ api-graphql/server.ts
@@ -3,8 +3,10 @@ import mount from 'koa-mount';
 import { graphqlHTTP } from 'koa-graphql';
 import schema from './schema/schema';
 import { initDB } from './database';
+import cors from '@koa/cors';
 
 const app = new Koa();
+app.use(cors());
 initDB()
 
 app.use(mount('/graphql', graphqlHTTP({
