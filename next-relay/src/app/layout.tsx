01ce9b6680aa6878a6dcf1cc0b253a047ca17536
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-04-14 00:05:45 -0300

    feat: set tailwind and client side home

--- next-relay/src/app/layout.tsx
+++ next-relay/src/app/layout.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link'
 import { ReactNode } from 'react'
+import './global.css'
+
 
 export const metadata = {
   title: 'Next Relay App',
@@ -8,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+{/*       <header>
+        <h1>Home</h1>
+        Insine nav className, will be the navigation bar styles
+        <nav className='bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto'>
+          <ul className=''>
+            <Link href="/dashboard">Dashboard</Link>
+          </ul>
+        </nav>
+      </header> */}
       <body>{children}</body>
     </html>
   )
--- next-relay/src/app/page.tsx
+++ next-relay/src/app/page.tsx
@@ -1,46 +1,131 @@
-import Link from 'next/link'
+'use client'
+
 import { initRelayEnvironment } from '../RelayEnvironment'
 
-import { graphql, fetchQuery } from "relay-runtime";
+import { graphql, fetchQuery, commitMutation } from "relay-runtime";
 
 import { pageUserQuery } from '../../__generated__/pageUserQuery.graphql';
+import { FormEvent, useRef, useState } from 'react';
 
-const relaySetup = async () => {
-  const environment = initRelayEnvironment();
+const Home = () => {
+  const [user, setUser] = useState<pageUserQuery['response']['user']>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  //Define the query Type and the query
+  const nameRef = useRef<HTMLInputElement>(null)
+  const birthDateRef = useRef<HTMLInputElement>(null)
+  const motherNameRef = useRef<HTMLInputElement>(null)
+  const phoneRef = useRef<HTMLInputElement>(null)
 
-  const query = graphql`
-    query pageUserQuery($id: String!) {
-      user(id: $id) {
-        id,
-        name,
-        birth_date,
-        mother_name,
-        phone
+  const relayQuerySetup = async (id : string) => {
+
+    //Define the query Type and the query
+    const environment = initRelayEnvironment();
+    const query = graphql`
+      query pageUserQuery($id: String!) {
+        user(id: $id) {
+          id,
+          name,
+          birth_date,
+          mother_name,
+          phone
+        }
       }
-    }
-  `
-  // Execute the query.
-  const observable = fetchQuery<pageUserQuery>(environment, query, {id: '642e382f986b81f7bc030ffd'});
-  const data = await observable.toPromise()
+    `
+    // Execute the query without cors
+    const observable = await fetchQuery<pageUserQuery>(environment, query, { id: id })
+    const data = await observable.toPromise()
+  
+    return data
+  }
 
-  return data
-}
+  const addUser = (e: FormEvent) => {
+    e.preventDefault()
 
-export default async function Home() {
-  const user = await relaySetup().then(data => data?.user)
+    const name = nameRef.current?.value
+    const birth_date = birthDateRef.current?.value
+    const mother_name = motherNameRef.current?.value
+    const phone = phoneRef.current?.value
+
+    const mutate = graphql`
+      mutation pageAddUserMutation($name: String!, $birth_date: String!, $mother_name: String!, $phone: String!) {
+        addUser(name: $name, birth_date: $birth_date, mother_name: $mother_name, phone: $phone) {
+          id,
+          name,
+          birth_date,
+          mother_name,
+          phone
+        }
+      }
+    `
+
+    commitMutation(
+      initRelayEnvironment(),
+      {
+        mutation: mutate,
+        variables: {
+          name: name!,
+          birth_date: birth_date!,
+          mother_name: mother_name!,
+          phone: phone!
+        },
+        onCompleted: (data: any) => {
+          console.log(data)
+          setUser(data.addUser)
+        }
+      }
+    )
+  }
+
+  const search = () => {
+    const id = inputRef.current?.value
+    relayQuerySetup(id!).then(data => {
+      setUser(data!.user)
+    })
+  }
 
   return (
-    <div>
-      <h1>Hello Dash</h1>
-        <div key={user?.id}>
-          <h1>{user?.name}</h1>
-          <p>{user?.birth_date}</p>
-          <p>{user?.mother_name}</p>
-          <p>{user?.phone}</p>
+    <div className='m-20'>      
+      <div className='flex gap-2 justify-end'>
+        <input className='border border-solid border-black rounded' ref={inputRef} type="text" />
+        <button onClick={search}>Search</button>
+      </div>
+
+      <form onSubmit={e => addUser(e)} className='flex flex-col gap-4 mt-8 items-center'>
+        <h1 className='text-xl'>Add User</h1>
+
+        <div className='flex justify-between w-80'>
+          <label htmlFor="name">Name</label>
+          <input className='border border-solid border-black rounded' id='name' ref={nameRef} type="text" />
         </div>
-      Link to <Link href="/dashboard">Dashboard</Link>
+
+        <div className='flex justify-between w-80'>
+          <label htmlFor="birth_date">Birth Date</label>
+          <input className='border border-solid border-black rounded' id='birth_date' ref={birthDateRef} type="text" />
+        </div>
+
+        <div className='flex justify-between w-80'>
+          <label htmlFor="mother_name">Mother Name</label>
+          <input className='border border-solid border-black rounded' id='mother_name' ref={motherNameRef} type="text" />
+        </div>
+
+        <div className='flex justify-between w-80'>
+          <label htmlFor="phone">Phone</label>
+          <input className='border border-solid border-black rounded' id='phone' ref={phoneRef} type="text" />
+        </div>
+        <button className='flex justify-center w-40' type="submit">Add User</button>
+      </form>
+
+      {user ? (
+        <div>
+          <h2>{user.name}</h2>
+          <h2>{user.birth_date}</h2>
+          <h2>{user.mother_name}</h2>
+          <h2>{user.phone}</h2>
+        </div>
+      ): null}
+      
     </div>
   )
 }
+
+export default Home;
